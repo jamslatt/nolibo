24e4b0d5b761c08f894fff5699fc77b8a599c868
James Slattery <james@slattery.tech>
2018-11-19 11:31:24 -0800

    Server account perms

--- server/main.js
+++ server/main.js
@@ -1,8 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  Roles.addUsersToRoles('xNG7oQm47wyBpiRce', ['admin','super-admin'])
+  Roles.addUsersToRoles('Gk8qdHDm9Jev8CQKK', ['admin'])
+  Roles.addUsersToRoles('NmdYk9hdiRYWj2a26', ['admin','super-admin'])
 
+  Accounts.config({
+  forbidClientAccountCreation : true
+});
 
 
 });
