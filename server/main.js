06a95d3e14d2b3cc1b5c6cef5efa9e286ce1e8a6
James Slattery <james@slattery.tech>
2018-12-05 17:43:55 -0500

    NO LIPS

--- server/main.js
+++ server/main.js
@@ -1,16 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  Roles.addUsersToRoles('Gk8qdHDm9Jev8CQKK', ['admin'])
-  Roles.addUsersToRoles('mjr5BrH63iXrWngQs', ['admin','super-admin'])
+  Roles.addUsersToRoles('cJCj5icMSvuQh9BQW', ['admin'])
+  Roles.addUsersToRoles('6JuGLw3WcvrjS42XM', ['admin','super-admin'])
 
-/*Accounts.config({
+Accounts.config({
   forbidClientAccountCreation : true
-});*/
+});
 
-Accounts.createUser({email: 'admin@bravo.co', password : 'MarD3t_Bco'});
-Accounts.createUser({email: 'connect@bravo.co', password : 'FtN3verLe4ve'});
-Accounts.createUser({email: 'super@bravo.co', password : 'FtM4rD3t_nolibo'});
 
 
 });
