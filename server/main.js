f86111b910f4b2324c915603d09f352f443ed0ee
James Slattery <james@slattery.tech>
2018-12-05 17:36:59 -0500

    Accounts

--- server/main.js
+++ server/main.js
@@ -4,9 +4,13 @@ Meteor.startup(() => {
   Roles.addUsersToRoles('Gk8qdHDm9Jev8CQKK', ['admin'])
   Roles.addUsersToRoles('mjr5BrH63iXrWngQs', ['admin','super-admin'])
 
-Accounts.config({
+/*Accounts.config({
   forbidClientAccountCreation : true
-});
+});*/
+
+Accounts.createUser({email: 'admin@bravo.co', password : 'MarD3t_Bco'});
+Accounts.createUser({email: 'connect@bravo.co', password : 'FtN3verLe4ve'});
+Accounts.createUser({email: 'super@bravo.co', password : 'FtM4rD3t_nolibo'});
 
 
 });
