5f2efccb02ebfc6392049fb1d439aad31185c732
James <me@james.run>
2020-09-16 20:07:48 -0700

    update

--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  Roles.addUsersToRoles('cJCj5icMSvuQh9BQW', ['admin'])
-  Roles.addUsersToRoles('6JuGLw3WcvrjS42XM', ['admin','super-admin'])
+  //Roles.addUsersToRoles('cJCj5icMSvuQh9BQW', ['admin'])
+  //Roles.addUsersToRoles('6JuGLw3WcvrjS42XM', ['admin','super-admin'])
 
 Accounts.config({
   //forbidClientAccountCreation : true
